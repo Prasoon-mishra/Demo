87e6d163c4d9b14d5d25118bbba5638708fa5cd9
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-08-11 10:41:30 +0530

    [Prasoon] Add UC1 attendance program

---
+++ Demo/UC1-demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class UC1_demo
+    {
+        public UC1_demo()
+        {
+            Console.WriteLine("Demo UC1");
+        }
+    }
+}
